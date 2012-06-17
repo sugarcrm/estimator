cbace70706ca5d3ea8e1f37a25cb2115d6db9334
Carl Hicks <carl.hicks@gmail.com>
2012-06-17 23:42:59 +0200

    Better event handling

--- public/assets/js/application.js
+++ public/assets/js/application.js
@@ -27,27 +27,44 @@ SugarCRM.Views.Calculator = Backbone.View.extend({
 })
 
 SugarCRM.Views.Topology = Backbone.View.extend({
-  initialize: function(parameters) {
-    this.parameters = parameters;
+  template: _.template($('#template-topology').html()),
+  initialize: function() {
+    this.parameters = this.options.parameters;
+  },
+  render: function() {
+    // Compile the template
+    html = this.template({});
+    // Swap in the template
+    $(this.el).html(html);
   }
 })
 
 SugarCRM.Views.SugarCRM = Backbone.View.extend({
   el: $('#sugarcrm'),
   template: _.template($('#template-sugarcrm').html()),
-  initialize: function() {
+  events: {
+    "change .calculateable" : "updateParameters",
+  },
+  initialize: function() {    
     this.parameters     = new SugarCRM.Models.Parameters;
-    this.servers        = new SugarCRM.Collections.Servers(this.parameters);
+    this.parameters.bind('change', this.render, this);
+  },
+  updateParameters: function(e) {
+    field = e.currentTarget.id;
+    value = e.currentTarget.value;
+    console.log(field + " changed to: " + value);
+    this.parameters.set(field, value);
+    this.parameters.updateCalculatedFields();
   },
   render: function() {
     $(this.el).html(this.template({}));
     this.parametersView = new SugarCRM.Views.Parameters({ parameters: this.parameters, el: this.$('#parameters') });
     this.calculatorView = new SugarCRM.Views.Calculator({ parameters: this.parameters, el: this.$('#calculations') });
-    //this.topologyView   = new SugarCRM.Views.Topology(this.parameters);
+    this.topologyView   = new SugarCRM.Views.Topology({ parameters: this.parameters, el: this.$('#topology') });
 
     this.parametersView.render();
     this.calculatorView.render();
-    //this.topologyView.render();
+    this.topologyView.render();
   },
 })
 
